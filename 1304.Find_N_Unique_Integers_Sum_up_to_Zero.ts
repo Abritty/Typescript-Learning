b5cd7ad606402dae30946b034c85dbd017348946
Shafia Afrin <shafia.afrincseruet15@gmail.com>
2025-09-07 14:14:35 +0200

    Create 1304.Find_N_Unique_Integers_Sum_up_to_Zero.ts

---
+++ 1304.Find_N_Unique_Integers_Sum_up_to_Zero.ts
@@ -0,0 +1,19 @@
+function sumZero(n: number): number[] {
+    const res: number[] = []; 
+
+    if (n % 2 === 1) {
+       res.push(0);
+       n -= 1; // reduce n to an even count to add symmetric pairs
+    }
+
+    for (let i: number = 1; i<=n/2; i++) {
+         res.push(i, -i);
+    }
+  
+  return res;
+};
+
+console.log(sumZero(5)); // Output: [-2, -1, 0, 1, 2]
+console.log(sumZero(0));
+console.log(sumZero(2));
+console.log(sumZero(1));
